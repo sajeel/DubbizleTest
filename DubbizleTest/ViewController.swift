8da98a556c70a6d4a5e80657bddb6559eb9c251d
Sajjeel <sajjeelh@qmic.com>
2021-12-04 10:07:42 +0300

    Initial Commit

---
+++ DubbizleTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DubbizleTest
+//
+//  Created by Sajjeel Hussain Khilji on 04/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
